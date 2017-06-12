54e4c2dbad7beb89346699c4efbe6644dd08c6bd
Stanley Tiu <stanleytiu7@gmail.com>
2017-06-11 23:57:57 -0400

    added some comments to the mergesort algo

--- bubblesort.js
+++ bubblesort.js
@@ -1,6 +1,9 @@
 function bubbleSort(array) {
+  //for all the indices
   for (var i = 0; i < array.length; i++){
+    //compare to the indices after it
     for (var j = i + 1; j < array.length; j++){
+      //swap if the right one is greater
       if (array[i] > array[j]){
         var swapped = swap(array[i], array[j]);
         array[i] = swapped[0];
@@ -10,7 +13,7 @@ function bubbleSort(array) {
   }
   return array;
 }
-
+// two value swap, easy to do if you use destructuring otherwise do it like this with a temp variable.
 function swap(arrI, arrJ) {
   var temp;
   temp = arrI;
@@ -28,20 +31,26 @@ function mergeSort(array, pushArray) {
   splitArray = split(array);
 
   // splitting stuff!!!!
+  // keep splitting while [0] and [1] are not single number arrays
   while(splitArray[0].length !== 1 || splitArray[1].length !== 1){
+    // [0] contains only 1 number, [1] contains multiple number array, push [0] to end
     if (splitArray[0].length === 1 && splitArray[1].length !== 1){ 
       splitArray.push(splitArray.shift());
     } 
     else {
-    var temp = split(splitArray.shift());
-    splitArray.push(temp[0]);
-    splitArray.push(temp[1]);
+      //split
+      var temp = split(splitArray.shift());
+      //push to end
+      splitArray.push(temp[0]);
+      splitArray.push(temp[1]);
     } 
   } 
 
   // putting it back together!!!
-  while(splitArray[0].length !== array.length){
+  // while [0].length is not original array length
+  while(splitArray[0].length !== array.length){ 
     splitArray.push(merge(splitArray));
+    //for getting rid of the extra stuff at the front (left and right used in merge)
     splitArray.shift();
     splitArray.shift();
   }
@@ -50,24 +59,31 @@ function mergeSort(array, pushArray) {
 }
 function split(array){
   var tempArray = [];
+  // will make left side array larger for odd numbers
   var middle = Math.ceil(array.length / 2);
   var left = array.slice(0, middle);
   tempArray.push(left);
   var right = array.slice(middle);
   tempArray.push(right);
+  // this is an array of 2 arrays, left and right halves
   return tempArray;
 }
 
 function merge(array){
   var mergeArray = [];
+  //take first and second index as left and right to be merged
   var left = array[0];
   var right = array[1];
 
+  //while both left and right arrays exist
   while (left.length > 0 || right.length > 0){
+    //if left and right both exist compare them and push value
     if(left[0] <= right[0]){
       mergeArray.push(left.length ? left.shift() : left);
     } else if (right[0] <= left[0]){
       mergeArray.push(right.length ? right.shift() : right);
+
+      //if left exists and right does not and viceversa
     } else if (left.length > 0 && right.length === 0){
       mergeArray.push(left.length ? left.shift() : left);
     } else if (right.length > 0 && left.length === 0){
